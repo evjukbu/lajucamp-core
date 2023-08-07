5d935e9704f8721238a02d59ffa2dea78679f25a
Jens <kjd@kirchenkreis-burgdorf.de>
2023-08-07 14:38:23 +0200

    prevent stuff being hidden below bottom navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <LayoutTopbar class="sticky top-0 z-50" />
-    <main class="content px-3 bg-background relative">
+    <main class="content px-3 bg-background relative mb-16">
         <slot />
     </main>
 <LayoutBottomNavigation />
