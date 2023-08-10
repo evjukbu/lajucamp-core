07b8ff825a1f7fbd179624385616461651010da7
Jens <kjd@kirchenkreis-burgdorf.de>
2023-08-10 23:28:42 +0200

    fix: theme not displayed correctly when content overflows screen size
    fix: layout is messed up when content is longer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,22 @@
 <template>
     <LayoutThemeHelper>
-        <LayoutTopbar class="sticky top-0 z-50" />
-        <main class="content px-3 bg-background relative mb-16">
-            <slot />
-        </main>
-        <LayoutBottomNavigation />
+    <LayoutTopbar class="sticky top-0 z-50" />
+    <main :data-theme="cookie" class="content px-3 bg-background relative pb-16">
+        <slot />
+    </main>
+    <LayoutBottomNavigation />
     </LayoutThemeHelper>
 </template>
+
+<script setup>
+const cookie = useCookie("theme", { expires: new Date('9999-12-31') })
+
+function getCurrentThemeName() {
+    if (cookie.value === undefined) return "fantasy"
+    return cookie.value
+}
+
+onMounted(async () => {
+    cookie.value = getCurrentThemeName()
+})
+</script>
