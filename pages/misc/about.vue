4a18fbe974122efd2daaf8654f6584cc6cd59280
Jens <kjd@kirchenkreis-burgdorf.de>
2023-08-21 22:51:11 +0200

    add multiple people to the about dialog

--- pages/misc/about.vue
+++ pages/misc/about.vue
@@ -7,46 +7,137 @@
       <div class="card-body">
         <h2 class="card-title">Jugendfestival App Version {{ version }}</h2>
         <figure class="flex justify-center">
-          <img src="/icon.png" class="w-1/2" />
+          <img src="~assets/ejkkbu.png" class="w-1/3" />
         </figure>
         <span class="text-center">Copyright © 2023 Evangelische Jugend im Kirchenkreis Burgdorf.</span>
       </div>
     </div>
     <div class="card card-compact w-full bg-base-100 shadow-xl">
       <div class="card-body">
-        <h2 class="card-title">Autor</h2>
-        <div class="flex justify-center">
-          <div class="avatar placeholder">
-            <div class="bg-success text-base-100 rounded-full w-16">
-              <span class="text-2xl">JK</span>
+        <h2 class="card-title">Mitwirkende</h2>
+        <div class="flex flex-col space-y-1">
+          <div class="flex justify-center mb-3">
+            <div class="avatar placeholder">
+              <div class="bg-success text-base-100 rounded-full w-16">
+                <span class="text-2xl">JK</span>
+              </div>
             </div>
           </div>
-        </div>
-        <span class="text-center">Jens Krieger-Juhnke</span>
-        <div class="flex justify-center flex-row">
-          <a href="mailto:jens@evju-uetze.de">
-            <button class="btn btn-xl btn-ghost">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-              </svg>
+          <span class="text-center font-bold text-lg">Jens Krieger-Juhnke</span>
+          <span class="text-center text-sm font-light">Hauptentwickler</span>
+          <div class="flex justify-center flex-row">
+            <div class="join">
+              <a href="mailto:jens@evju-uetze.de">
+                <button class="btn btn-xl btn-ghost join-item">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                  </svg>
 
-            </button>
+                </button>
 
-          </a>
-          <a href="https://instagram.com/__sensens_">
-            <button class="btn btn-xl btn-ghost">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-              </svg>
+              </a>
+              <a href="https://instagram.com/__sensens_">
+                <button class="btn btn-xl btn-ghost join-item">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                  </svg>
 
-            </button>
+                </button>
 
 
-          </a>
+              </a>
+            </div>
+          </div>
+          <div>
+            <div class="flex flex-row py-1 space-x-2">
+              <div class="flex flex-col">
+                <div class="avatar placeholder">
+                  <div class="bg-red-400 text-base-100 rounded-full w-12">
+                    <span class="text-lg">AB</span>
+                  </div>
+                </div>
+              </div>
+              <div class="flex flex-col space-y-1">
+                <span class="font-bold">Anne Basedau <div class="badge badge-primary badge-outline">EvJu BuLa</div>
+                </span>
+                <span class="text-sm font-light">Projektbegleitung</span>
+              </div>
+              <div class="flex-grow"></div>
+              <a href="https://instagram.com/annebasedau">
+                <button class="btn btn-ghost">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                  </svg>
+
+
+                </button>
+              </a>
+
+            </div>
+
+          </div>
+          <div>
+            <div class="flex flex-row py-1 space-x-2">
+              <div class="flex flex-col">
+                <div class="avatar placeholder">
+                  <div class="bg-secondary text-base-100 rounded-full w-12">
+                    <span class="text-lg">AB</span>
+                  </div>
+                </div>
+              </div>
+              <div class="flex flex-col space-y-1">
+                <span class="font-bold">Anna Brandt <div class="badge badge-primary badge-outline">EvJu Celle</div></span>
+                <span class="text-sm font-light">Testerin und Begleitung für IOS</span>
+              </div>
+              <div class="flex-grow"></div>
+              <a href="https://instagram.com/anna.c.brandt">
+                <button class="btn btn-ghost">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                  </svg>
+
+
+                </button>
+              </a>
+            </div>
+          </div>
+          <div>
+            <div class="flex flex-row py-1 space-x-2">
+              <div class="flex flex-col">
+                <div class="avatar placeholder">
+                  <div class="bg-orange-400 text-base-100 rounded-full w-12">
+                    <span class="text-lg">AR</span>
+                  </div>
+                </div>
+              </div>
+              <div class="flex flex-col space-y-1">
+                <span class="font-bold">Anni Reimann <div class="badge badge-primary badge-outline">EvJu Burgdorf</div>
+                </span>
+                <span class="text-sm font-light">Projektbegleitung</span>
+              </div>
+              <div class="flex-grow"></div>
+              <a href="https://instagram.com/annimation85">
+                <button class="btn btn-ghost">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                  </svg>
+
+
+                </button>
+              </a>
+
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -70,10 +161,19 @@ const version = "1.0"
 
 const changelog = [
   {
+    date: "21. August 2023",
+    changes: [
+      "Added: In der Webversion wird auf der Startseite nun eine 'App installieren' Schaltflächen angezeigt.",
+      "Added: Eine Anleitung zum Installieren der App auf IOS Geräten wurde hinzugefügt.",
+      "Added: Mehrere Mitwirkende können unter 'Über diese App' angezeigt werden.",
+      "Change: 'Über diese App' wurde optisch angepasst."
+    ]
+  },
+  {
     date: "16. August 2023",
     changes: [
-      "change: Das Festival Logo wird nun unter 'Über diese App' verwendet.",
-      "change: Die aktuelle Version des Festival Logos wird verwendet."
+      "Change: Das Festival Logo wird nun unter 'Über diese App' verwendet.",
+      "Change: Die aktuelle Version des Festival Logos wird verwendet."
     ]
   },
   {
