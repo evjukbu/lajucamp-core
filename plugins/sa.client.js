9dfd2dca3552cd8c93828ebdc679dc234999a33a
Jens <kjd@kirchenkreis-burgdorf.de>
2023-09-06 21:35:56 +0000

    fix: sa not working

--- plugins/sa.client.js
+++ plugins/sa.client.js
@@ -2,6 +2,5 @@ import SimpleAnalytics from "simple-analytics-vue";
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(SimpleAnalytics, {
-        skip: process.env.NODE_ENV !== "production"
     });
 });
