1938c4e8d7ac709ec05233baf5ad17e16e240491
Jens <kjd@kirchenkreis-burgdorf.de>
2023-09-06 21:48:04 +0000

    use custom sa domain

--- plugins/sa.client.js
+++ plugins/sa.client.js
@@ -1,6 +1,8 @@
 import SimpleAnalytics from "simple-analytics-vue";
-
 export default defineNuxtPlugin((nuxtApp) => {
+    const config = useRuntimeConfig();
+
     nuxtApp.vueApp.use(SimpleAnalytics, {
+        domain: config.public.saDomain,
     });
 });
