f65465db9984a47f440b11cbbb6079cf15bb3afe
Jens Krieger-Juhnke <kjd@kirchenkreis-burgdorf.de>
2023-12-05 16:36:48 +0000

    feat: allow creation of events from Backend

--- components/AdminLayoutHeader.vue
+++ components/AdminLayoutHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pl-3">
+  <div class="pl-3 pr-3">
     <div class="text-sm breadcrumbs">
       <ul>
         <li v-for="element in props.path">
@@ -10,24 +10,28 @@
         </li>
       </ul>
     </div>
-    <div class="flex flex-row space-x-2">
-      <h1 class="text-3xl">{{ title }}</h1>
-      <div class="tooltip tooltip-bottom pt-2" :data-tip="subtitle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-          />
-        </svg>
+    <div class="flex flex-row justify-between">
+      <div class="flex flex-row space-x-2">
+        <h1 class="text-3xl">{{ title }}</h1>
+        <div class="tooltip tooltip-bottom pt-2" :data-tip="subtitle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+            />
+          </svg>
+        </div>
       </div>
+      <slot />
+
     </div>
     <div class="pb-8" />
   </div>
--- pages/admin/events/index.vue
+++ pages/admin/events/index.vue
@@ -7,7 +7,165 @@
       { name: 'Home', path: '/admin', active: false },
       { name: 'Veranstaltungen', path: '/admin/events', active: true },
     ]"
-  />
+  >
+    <button class="btn btn-primary" @click="openCreateDialog()">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        class="w-5 h-5"
+      >
+        <path
+          d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"
+        />
+      </svg>
+      Neue Veranstaltung
+    </button>
+    <dialog id="my_modal_1" class="modal">
+      <div class="modal-box w-11/12 max-w-7xl">
+        <button
+          class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+          @click="closeDialog()"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
+            />
+          </svg>
+        </button>
+        <h3 class="font-bold text-lg">Neue Veranstaltung</h3>
+        <div class="py-4">
+          <div class="label">
+            <span class="label-text">Veranstaltungstitel</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Beispieltitel"
+            class="input input-bordered w-full"
+            :disabled="submitting"
+            v-model="name"
+          />
+          <div class="flex flex-row space-x-3 my-3">
+            <div class="w-full">
+              <div class="label">
+                <span class="label-text">Startzeitpunkt</span>
+              </div>
+              <VueDatePicker
+                class="w-full"
+                v-model="start"
+                input-class-name="input input-bordered"
+                menu-class-name="menu"
+                :disabled="submitting"
+              ></VueDatePicker>
+            </div>
+            <div class="w-full">
+              <div class="label">
+                <span class="label-text">Endzeitpunkt</span>
+              </div>
+              <VueDatePicker
+                class="w-full"
+                v-model="end"
+                input-class-name="input input-bordered"
+                menu-class-name="menu"
+                :disabled="submitting"
+              ></VueDatePicker>
+            </div>
+          </div>
+          <div class="flex flex-row space-x-3">
+            <label class="form-control w-full">
+              <div class="label">
+                <span class="label-text">Kategorie</span>
+              </div>
+              <select
+                v-model="category"
+                class="select select-bordered"
+                :disabled="submitting"
+              >
+                <option
+                  v-for="category in categories"
+                  :key="category.id"
+                  :value="category.id"
+                >
+                  {{ category.name }}
+                </option>
+              </select>
+            </label>
+            <label class="form-control w-full">
+              <div class="label">
+                <span class="label-text">Ort</span>
+              </div>
+              <select
+                v-model="location"
+                class="select select-bordered"
+                :disabled="submitting"
+              >
+                <option
+                  v-for="location in locations"
+                  :key="location.id"
+                  :value="location.id"
+                >
+                  {{ location.name }}
+                </option>
+              </select>
+            </label>
+          </div>
+          <div class="label">
+            <span class="label-text">Beschreibung</span>
+          </div>
+          <quill-editor
+            class="h-48 min-h-48 pt-3"
+            theme="snow"
+            toolbar="full"
+            contentType="html"
+            v-model:content="description"
+            :enable="!submitting"
+          ></quill-editor>
+          <div class="form-control">
+            <label class="label cursor-pointer">
+              <span class="label-text">Auf der Startseite anzeigen:</span>
+              <input
+                type="checkbox"
+                class="toggle"
+                v-model="show_homepage"
+                false-value="false"
+                true-value="true"
+                checked
+                :disabled="submitting"
+              />
+            </label>
+          </div>
+        </div>
+        <div class="modal-action">
+          <button class="btn pr-3" :disabled="submitting" @click="closeDialog()">
+            Abbrechen
+          </button>
+          <button
+            class="btn btn-primary"
+            :disabled="
+              name === null ||
+              start === null ||
+              end === null ||
+              location === null ||
+              category === null ||
+              submitting
+            "
+            @click="saveNew()"
+          >
+            <div v-if="submitting">
+              <span v-if="submitting" class="loading loading-spinner loading-xs"></span>
+              Übertragen...
+            </div>
+            <div v-else>Speichern</div>
+          </button>
+        </div>
+      </div>
+    </dialog>
+  </AdminLayoutHeader>
   <div class="pl-3">
     <div class="overflow-x-auto">
       <table class="table">
@@ -142,6 +300,11 @@
 </template>
 
 <script setup>
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
+
 definePageMeta({
   middleware: "auth",
   layout: "admin",
@@ -153,6 +316,13 @@ data.value = await pb.collection("events").getFullList({
   filter: 'team="' + pb.authStore.model.team + '"',
   expand: "location,category",
 });
+const categories = await pb.collection("categories").getFullList({
+  sort: "name",
+});
+const locations = await pb.collection("locations").getFullList({
+  sort: "name",
+});
+
 /*
 const formatter = Intl.DateTimeFormat("de-DE", {
   weekday: "long",
@@ -162,4 +332,57 @@ const formatter = Intl.DateTimeFormat("de-DE", {
   minute: "numeric",
   timeZone: "Europe/Berlin",
 });*/
+
+const name = ref(null);
+const category = ref(null);
+const location = ref(null);
+const start = ref(null);
+const end = ref(null);
+const description = ref(null);
+const show_homepage = ref(true);
+const team = pb.authStore.model.team;
+const submitting = ref(false);
+
+function reset() {
+  name.value = null;
+  category.value = null;
+  location.value = null;
+  start.value = null;
+  end.value = null;
+  description.value = null;
+  show_homepage.value = true;
+}
+
+function assembleEvent() {
+  return {
+    name: name.value,
+    category: category.value,
+    location: location.value,
+    start: start.value,
+    end: end.value,
+    description: description.value,
+    homepage_ignore: !show_homepage.value,
+    team: team,
+  };
+}
+function openCreateDialog() {
+  my_modal_1.showModal();
+}
+
+function closeDialog() {
+  reset();
+  my_modal_1.close();
+}
+
+async function saveNew() {
+  submitting.value = true;
+  const created = await pb.collection("events").create(assembleEvent());
+  submitting.value = false;
+  const record = await pb.collection("events").getOne(created.id, {
+    expand: "location,category",
+  });
+  data.value.push(record);
+  closeDialog();
+  return record;
+}
 </script>
