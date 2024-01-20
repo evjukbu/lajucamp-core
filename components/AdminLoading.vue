c8d3eb9ee9a7c070876f7431f9ddec0db55850de
Jens Krieger-Juhnke <kjd@kirchenkreis-burgdorf.de>
2024-01-20 10:49:25 +0000

    feat: loading page for AdminEventList

---
+++ components/AdminLoading.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="hero">
+        <div class="hero-content text-center">
+          <div class="max-w-md">
+            <span class="loading loading-spinner loading-lg"></span>
+            <h1 class="text-2xl">Wir laden die letzten Daten für dich...</h1>
+          </div>
+        </div>
+      </div>
+</template>
