ee3ad44f7991f5752ad2d11e8db2b9534825b70a
Jan <55839828+Piitschy@users.noreply.github.com>
2024-01-28 14:00:29 +0100

    Update KeyScanner.spec.ts

--- tests/KeyScanner.spec.ts
+++ tests/KeyScanner.spec.ts
@@ -2,9 +2,9 @@ import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 
 import KeyScanner from '../components/KeyScanner.vue'
-const wrapper = mount(KeyScanner)
 
-describe('KeyScanner', () => {
+describe('KeyScanner default', () => {
+  const wrapper = mount(KeyScanner)
   it('is a Vue instance', () => {
     expect(wrapper.vm).toBeTruthy()
   })
@@ -13,5 +13,12 @@ describe('KeyScanner', () => {
     expect(wrapper.find('video').exists()).toBe(true)
   })
 
+  it('show settings', () => {
+    // not testable because dialog is hidden with css
+    expect(wrapper.find('dialog').exists()).toBe(true)
+    expect(wrapper.find('dialog').classes()).toContain('modal')
+    ////wrapper.findAll('button')[0].trigger('click')
+    ////expect(wrapper.find('dialog').isVisible()).toBe(true)
+  })
 
 })
